d2d95e52b41cd02c747968a958711e51436b2812
BenLooper <benlooper08@gmail.com>
2020-09-29 14:44:47 -0500

    added explore page with links to personal pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,19 @@ class Post < ApplicationRecord
     belongs_to :user
     belongs_to :genre
     has_many :comments
+    has_many :snaps
     has_many :slaps 
     has_many :backs
+
+    def user_name
+        self.user.username
+    end 
+
+    def snap_number
+        self.snaps.count.to_s
+    end 
+
+    def slap_number
+        self.slaps.count.to_s
+    end 
 end
