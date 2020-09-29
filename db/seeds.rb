571a6199b12b13e6d7bde07e5d67aba9af86c9da
BenLooper <benlooper08@gmail.com>
2020-09-29 11:11:38 -0500

    Seeded DB with filler data

---
+++ db/seeds.rb
@@ -0,0 +1,59 @@
+User.destroy_all
+Post.destroy_all
+Genre.destroy_all
+Comment.destroy_all
+Snap.destroy_all
+Slap.destroy_all
+
+
+
+# Users 
+10.times do                                                    
+  User.create(
+    username:Faker::Artist.name,
+    password:Faker::Number.number,
+    bio:Faker::Lorem.paragraph
+  )
+end 
+
+# Genres
+10.times do 
+    Genre.create(
+        style:Faker::Nation.nationality
+    )
+end 
+
+#Posts
+10.times do 
+    Post.create(
+    image_url:Faker::LoremPixel.image,
+    description:Faker::Lorem.sentence, 
+    user:User.all.sample, 
+    genre:Genre.all.sample
+    )
+end
+
+#Comments
+10.times do 
+    Comment.create(
+        user:User.all.sample,
+        post:Post.all.sample
+    )
+end 
+
+#Snaps
+10.times do 
+    Snap.create(
+        user:User.all.sample,
+        post:Post.all.sample
+    )
+end 
+
+#Slaps
+10.times do 
+    Slap.create(
+        user:User.all.sample,
+        post:Post.all.sample
+    )
+end 
+
