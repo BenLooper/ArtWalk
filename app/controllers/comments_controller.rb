f92fa4be69528b53aa127a2044617ebd44a5dc9a
BenLooper <benlooper08@gmail.com>
2020-10-01 06:46:28 -0500

    Added comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,13 @@
 class CommentsController < ApplicationController
-
-    def new 
-        @comment = Comment.new
-    end 
+    before_action :authenticated?
 
     def create 
-        
+        @user.comments.build(content:comment_params[:content], post_id:comment_params[:post_id])
+        redirect_to comment_params[:origin]
     end 
 
+
+    def comment_params 
+        params.require(:comment).permit(:content,:post_id, :origin)
+    end 
 end
