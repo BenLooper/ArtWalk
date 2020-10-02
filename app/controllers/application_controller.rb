faaefc647091a963531fbc47091fa90c077f5c43
BenLooper <benlooper08@gmail.com>
2020-10-02 11:21:21 -0500

    Centered content in every page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
     private
     def authenticated?
-        if session[:id] != nil
+        if session[:id] && session[:id] != nil
             @user = User.find(session[:id])
         else 
             redirect_to sign_in_path
